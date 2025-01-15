b14a46f372034d80594b5115cfcdefd0a4f71ca8
Xander Dundon <xander.dundon@gmail.com>
2025-01-16 02:16:18 +1100

    Basic file struct

--- secret-santa/src/main/java/com/santa/Main.java
+++ secret-santa/src/main/java/com/santa/Main.java
@@ -1,11 +1,18 @@
 package com.santa;
 
+import com.santa.Pages.Dashboard;
+
 import io.javalin.Javalin;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
+        
+
+
         var app = Javalin.create(/*config*/)
             .get("/", ctx -> ctx.html("Hello World"))
+            .get("/dashboard", ctx -> ctx.html(new Dashboard().html))
+            .error(404, ctx -> ctx.html("Page not found!"))
             .start(7070);
     }
 }
